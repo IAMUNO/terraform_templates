4db175adddd2ffddb02f5f12fb9145caab0fa35f
Yoo Yunho <yyh3690@naver.com>
2024-05-16 14:38:24 +0900

    gcp compute_backend_service variables.tf 작성중

--- gcp/compute_engine/backend_service/variables.tf
+++ gcp/compute_engine/backend_service/variables.tf
@@ -1,6 +1,43 @@
 variable "compute_backend_service" {
     type = map(object({
         default_service_name = string # re
+        
+        affinity_cookie_ttl_sec = string # op
+        backend = object({
+            balancing_mode = string # UTILIZATION, RATE, CONNECTION
+            capacity_scaler = string # op 
+            group = string # re
+            max_connections = string # op
+            max_connections_per_instance = string # op
+            max_connections_per_endpoint = string # op
+            max_rate = string # op
+            max_rate_per_instance = string # op
+            max_rate_per_endpoint = string # op
+            max_utilization = string # op
+        })
+        circuit_breakers = {
+            max_requests_per_connection = string # op
+            max_connections = string # op
+            max_pending_requests = string # op
+            max_requests = string # op
+            max_retries = string # op
+        }
+        compression_mode = string # AUTOMATIC, DISABLED
+        consistent_hash = object({
+            ttl_sec = string # re
+            ttl_nanos = string # op
+            http_cookie_name = string # op
+            http_cookie_path = string # op
+            http_header_name = string # op
+            minimum_ring_size = string # op
+        })
+        cdn_policy = object({
+           = 
+        })
+
+
+
+
         address = string # op
         address_type = string # INTERNAL, EXTERNAL
         purpose = string # op // GCE_ENDPOINT, SHARED_LOADBALANCER_VIP , VPC_PEERING , IPSEC_INTERCONNECT , PRIVATE_SERVICE_CONNECT
